6a62e57623fffd6da0560f64cab50cee8e705b7e
Andrew_Chaphiv <ac2gq@virginia.edu>
2023-08-16 12:20:46 -0400

    creation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='m09-demo',
+      version='0.1',
+      description='A simple demonstration package',
+      url='https://github.com/achapkim/DS5100_m09_exercise',
+      author='Andrew Chaphiv',
+      author_email='ac2gq@virginia.edu',
+      license='MIT',
+      packages=['demo'])
