f948c7da04891f6abdfcbc4199f73e15ba7ea8dd
Andrew_Chaphiv <ac2gq@virginia.edu>
2023-08-16 12:33:16 -0400

    edit

--- setup.py
+++ setup.py
@@ -6,5 +6,4 @@ setup(name='syrup',
       url='https://github.com/achapkim/DS5100_m09_exercise',
       author='Andrew Chaphiv',
       author_email='ac2gq@virginia.edu',
-      license='MIT',
-      packages=['demo'])
+      license='MIT')
--- syrup/__init__.py
+++ syrup/__init__.py
@@ -1,1 +1,2 @@
+import .syrup import add_one
 print("Welcome to my Module!")
